4f296f650f740654faf44481493c1d8f7ebe852b
Carter Johnston <CJohnston@seabest.com>
2023-06-15 12:56:06 -0400

    scaffolding for an editable table view

---
+++ src/lib/components/3092/EntryRow.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let firstName: string = '';
+	export let lastName: string | undefined = '';
+	export let ssn: string | undefined = '';
+	export let email: string | undefined = '';
+	export let country: string | undefined = '';
+	export let address: string | undefined = '';
+	export let city: string | undefined = '';
+	export let state: string | undefined = '';
+	export let zipcode: number | undefined = 0;
+
+	let ssnLeft = 0;
+	let ssnMid = 0;
+	let ssnRight = 0;
+
+	// TODO add constants files
+	const countries: { name: string; initial: string }[] = [
+		{ name: 'Afghanistan', initial: 'AF' },
+		{ name: 'Åland Islands', initial: 'AX' },
+		{ name: 'Albania', initial: 'AL' }
+	];
+	const states: { name: string; initial: string }[] = [
+		{ name: 'Florida', initial: 'FL' },
+		{ name: 'Georgia', initial: 'GA' }
+	];
+	const cities: string[] = ['Jacksonville', 'Tampa', 'Miami'];
+
+	$: ssn = `${ssnLeft}-${ssnMid}-${ssnRight}`;
+</script>
+
+<tr>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+</tr>
--- src/routes/3092idea/+page.svelte
+++ src/routes/3092idea/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Entry from '$lib/components/3092/Entry.svelte';
+	import EntryRow from "$lib/components/3092/EntryRow.svelte";
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -15,7 +15,7 @@
 		zipcode: number | undefined;
 	};
 
-	let entries: EntryProps[] = [];
+	let entries: string[] = [];
 
 	$: console.log(entries);
 </script>
@@ -23,38 +23,37 @@
 <h1 class="text-3xl text-center mt-10">3092</h1>
 
 <form method="POST">
-	{#each entries as entry}
-		<Entry {...entry} />
-	{/each}
 
-	<button
-		on:click={() => {
-			console.log('hello');
-			entries.push({
-				firstname: undefined,
-				lastname: undefined,
-				ssn: undefined,
-				email: undefined,
-				address: undefined,
-				city: undefined,
-				state: undefined,
-				zipcode: undefined
-			});
-			entries = entries;
-		}}
-		type="button">New Entry</button
-	>
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">First Name</th>
+                <th scope="col" class="px-6 py-3">Last Name</th>
+                <th scope="col" class="px-6 py-3">Social Security No.</th>
+                <th scope="col" class="px-6 py-3">Email</th>
+                <th scope="col" class="px-6 py-3">Address</th>
+                <th scope="col" class="px-6 py-3">City</th>
+                <th scope="col" class="px-6 py-3">State</th>
+                <th scope="col" class="px-6 py-3">Zipcode</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each entries as entry}
+                <EntryRow />
+            {/each}
+        </tbody>
+    </table>
 
 	<div class="space-y-12 sm:space-y-16">
 		<div class="mt-6 flex items-center justify-end gap-x-6">
-			<button type="button" class="text-sm font-semibold leading-6 text-gray-900 mb-10">
-				Cancel
-			</button>
-			<button
-				type="submit"
-				class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-10"
-			>
-				Submit
+			<button class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-10"
+                    type="button"
+                    on:click={() => {
+                        console.log('hello');
+                        entries.push("");
+                        entries = entries;
+                    }}>
+				+ New Row
 			</button>
 		</div>
 	</div>
