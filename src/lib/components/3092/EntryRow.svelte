3187755d805745f35e992b4b17fa8468d023727f
Carter <johnston.j.carter@gmail.com>
2023-06-15 22:15:42 -0400

    WIP

--- src/lib/components/3092/EntryRow.svelte
+++ src/lib/components/3092/EntryRow.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	export let index: number;
+
 	export let firstName: string = '';
-	export let lastName: string | undefined = '';
-	export let ssn: string | undefined = '';
-	export let email: string | undefined = '';
-	export let country: string | undefined = '';
-	export let address: string | undefined = '';
-	export let city: string | undefined = '';
-	export let state: string | undefined = '';
-	export let zipcode: number | undefined = 0;
+	export let lastName: string = '';
+	export let ssn: string = '';
+	export let email: string = '';
+	export let country: string = '';
+	export let address: string = '';
+	export let city: string = '';
+	export let state: string = '';
+	export let zipcode: number = 0;
 
 	let ssnLeft = 0;
 	let ssnMid = 0;
@@ -28,13 +30,25 @@
 	$: ssn = `${ssnLeft}-${ssnMid}-${ssnRight}`;
 </script>
 
-<tr>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
-    <td bind:textContent={firstName} contenteditable>{firstName}</td>
+<tr class="">
+	<th scope="row">{index}</th>
+	<td class="px-6 py-0" bind:textContent={firstName} contenteditable>{firstName}</td>
+	<td class="px-6 py-0" bind:textContent={lastName} contenteditable>{lastName}</td>
+	<td class="flex flex-row px-2 py-0">
+		<div class="flex-none w-20">
+			<input bind:value={ssnLeft} type="number" />
+		</div>
+		<div class="flex-none w-20">
+			<input bind:value={ssnMid} type="number" />
+		</div>
+		<div class="flex-none w-20">
+			<input bind:value={ssnRight} type="number" />
+		</div>
+	</td>
+	<td class="px-6 py-0" bind:textContent={email} contenteditable>{email}</td>
+	<td class="px-6 py-0" bind:textContent={country} contenteditable>{country}</td>
+	<td class="px-6 py-0" bind:textContent={address} contenteditable>{address}</td>
+	<td class="px-6 py-0" bind:textContent={city} contenteditable>{city}</td>
+	<td class="px-6 py-0" bind:textContent={state} contenteditable>{state}</td>
+	<td class="px-6 py-0"><input bind:value={zipcode} type="number" /></td>
 </tr>
--- src/routes/3092idea/+page.svelte
+++ src/routes/3092idea/+page.svelte
@@ -1,60 +1,65 @@
 <script lang="ts">
-	import EntryRow from "$lib/components/3092/EntryRow.svelte";
+	import EntryRow from '$lib/components/3092/EntryRow.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	type EntryProps = {
-		firstname: string | undefined;
-		lastname: string | undefined;
-		ssn: string | undefined;
-		email: string | undefined;
-		address: string | undefined;
-		city: string | undefined;
-		state: string | undefined;
-		zipcode: number | undefined;
-	};
-
-	let entries: string[] = [];
+	let entries: any[] = [];
 
 	$: console.log(entries);
 </script>
 
-<h1 class="text-3xl text-center mt-10">3092</h1>
-
 <form method="POST">
-
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">First Name</th>
-                <th scope="col" class="px-6 py-3">Last Name</th>
-                <th scope="col" class="px-6 py-3">Social Security No.</th>
-                <th scope="col" class="px-6 py-3">Email</th>
-                <th scope="col" class="px-6 py-3">Address</th>
-                <th scope="col" class="px-6 py-3">City</th>
-                <th scope="col" class="px-6 py-3">State</th>
-                <th scope="col" class="px-6 py-3">Zipcode</th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each entries as entry}
-                <EntryRow />
-            {/each}
-        </tbody>
-    </table>
+	<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+		<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+			<tr>
+				<th scope="col" class="px-6 py-1">#</th>
+				<th scope="col" class="px-6 py-1">First Name</th>
+				<th scope="col" class="px-6 py-1">Last Name</th>
+				<th scope="col" class="px-6 py-1">Social Security No.</th>
+				<th scope="col" class="px-6 py-1">Email</th>
+				<th scope="col" class="px-6 py-1">Country</th>
+				<th scope="col" class="px-6 py-1">Address</th>
+				<th scope="col" class="px-6 py-1">City</th>
+				<th scope="col" class="px-6 py-1">State</th>
+				<th scope="col" class="px-6 py-1">Zipcode</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each entries as entry, index}
+				<EntryRow {index} bind:entries={...entry} />
+			{/each}
+		</tbody>
+	</table>
 
 	<div class="space-y-12 sm:space-y-16">
 		<div class="mt-6 flex items-center justify-end gap-x-6">
-			<button class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-10"
-                    type="button"
-                    on:click={() => {
-                        console.log('hello');
-                        entries.push("");
-                        entries = entries;
-                    }}>
+			<button
+				class="inline-flex justify-center rounded-md bg-indigo-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-10"
+				type="button"
+				on:click={() => {
+					entries.push({
+						firstName: '',
+						lastName: '',
+						ssn: '',
+						email: '',
+						country: '',
+						address: '',
+						city: '',
+						state: '',
+						zipcode: 0
+					});
+					entries = entries;
+				}}
+			>
 				+ New Row
 			</button>
 		</div>
 	</div>
 </form>
+
+<div class="container mx-auto bg-midnight px-4">
+	{#each entries as entry}
+		<p>{entry.firstName}{entry.lastName}</p>
+	{/each}
+</div>
