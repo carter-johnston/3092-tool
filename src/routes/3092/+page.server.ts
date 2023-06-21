e8d3f26a5463478517516d2cd443a3433bcbd5a4
Carter <johnston.j.carter@gmail.com>
2023-06-20 22:03:30 -0400

    dashboard grouping

--- src/routes/3092/+page.server.ts
+++ src/routes/3092/+page.server.ts
@@ -6,10 +6,17 @@ export const load: PageServerLoad = async ({ locals }) => {
 	// const { session } = await locals.auth.validateUser();
 	// if (!session) throw redirect(302, '/login');
 
-	const cto3092 = await prisma.cto3092.findMany();
-	console.log(cto3092);
+	const cto3092List = await prisma.cto3092.findMany();
+	let cto3092Grouped = cto3092List.reduce((group, cto) => {
+		const { groupingName } = cto;		
+		group[groupingName] = group[groupingName] ?? [];
+		group[groupingName].push(cto);
+		return group;
+	}, {});
+
+	cto3092Grouped = Object.values(cto3092Grouped);
 
 	return {
-		cto3092
+		cto3092Grouped
 	};
 };
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,11 +1,6 @@
 import { fail, redirect } from '@sveltejs/kit';
 import { auth } from '$lib/server/lucia';
-import type { PageServerLoad, Actions } from './$types';
-
-// export const load: PageServerLoad = async ({ locals }) => {
-// 	const { session } = await locals.auth.validateUser();
-// 	if (session) throw redirect(302, '/');
-// };
+import type { Actions } from './$types';
 
 export const actions: Actions = {
 	default: async ({ request, locals }) => {
