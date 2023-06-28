c080b4e6c5e981bc82366197ace1ed74c0d4ab02
Dhori Mato <dhori.mato@gmail.com>
2023-06-28 11:56:04 -0400

    Changed the registration page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -10,16 +10,28 @@
 				<h3 class="text-xl font-medium text-gray-900 dark:text-white">Register new user</h3>
 				<Label class="space-y-2">
 					<span>Username</span>
-					<Input type="text" id="username" name="username" placeholder="dxmato" required />
+					<Input
+						type="text"
+						id="username"
+						name="username"
+						placeholder="Enter a username"
+						required
+					/>
 				</Label>
 				<Label class="space-y-2">
 					<span>Password</span>
-					<Input type="password" id="password" name="password" placeholder="•••••" required />
+					<Input
+						type="password"
+						id="password"
+						name="password"
+						placeholder="Enter a password"
+						required
+					/>
 				</Label>
-				<Label class="space-y-2">
+				<!-- <Label class="space-y-2">
 					<span>Role</span>
-					<Input type="text" id="role" name="role" placeholder="Admin" required />
-				</Label>
+					<Input type="text" id="role" name="role" placeholder="Enter" required />
+				</Label> -->
 				<Button type="submit" class="w-full">Create account</Button>
 			</form>
 		</Card>
