b6eb38f54ba8a1eddc78527af8c0650bd89c4672
Dhori Mato <dhori.mato@gmail.com>
2023-07-10 07:50:21 -0400

    Added 2901 :)

--- src/routes/3092/randomized/RandomizedForm.svelte
+++ src/routes/3092/randomized/RandomizedForm.svelte
@@ -5,19 +5,20 @@
 <form method="POST">
 	<div class="grid gap-6 mb-6 md:grid-cols-2">
 		<div>
-			<Label for="cert-card" class="mb-2">Cert Card ID</Label>
+			<Label for="cert-card" class="mb-2">Cert Card ID*</Label>
 			<Input type="text" name="cert-card" id="cert-card" placeholder="123456789" required />
 		</div>
 		<div>
-			<Label for="certPin" class="mb-2">Cert Card PIN</Label>
+			<Label for="certPin" class="mb-2">Cert Card PIN*</Label>
 			<Input type="text" name="certPin" id="certPin" placeholder="1234" required />
 		</div>
 		<div>
-			<Label for="email" class="mb-2">Email</Label>
+			<Label for="email" class="mb-2">Email*</Label>
 			<Input type="text" name="email" id="email" placeholder="test@test.com" required />
 		</div>
 		<div>
-			<Label for="cardNumber" class="mb-2">Number of Cards to Generate</Label>
+			<Label for="cardNumber" class="mb-2">Number of Cards to Generate*</Label>
+			<span />
 			<Input type="number" name="cardNumber" id="cardNumber" min="1" max="100" required />
 		</div>
 	</div>
